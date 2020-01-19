667c6f50a42aefaaa1168824eccdd713af8dfb0c
Brechje Krah <breggieboe@hotmail.com>
2020-01-19 23:50:16 +0100

    yay

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+$(function() {
+    $( "#header2" ).click(function() {
+        $( ".maintext" ).toggle();
+    });
+});
+
+
 
 window.onload = function() {
 
-$(".popup").click(function(){
-	// hide the popup
-	$('.popup').hide();
+// activate the close button
+$(".close-button").click(function(){
+// hide the popup
+$('.popup').hide();
 });
 
-$(".popup-item").click(function(){
+/* BVB when you click the popup it closes */
+$(".popup").click(function(){
+// hide the popup
+$('.popup').hide();
+});
 
+// when you click an image with class popup-item
+$(".popup-item").click(function(){
+  // hide popup in case one is open
+  // $('.popup').hide();
+// // get the id of the clicked menu item
+// var id = $(this).attr('id');
+  // // use it to open the connected popup
+// $("#popup-" + id).show();
+
+  // BVB get the image src of the clicked image
   var imagesrc = $(this).attr("src");
 
+  // BVB pass it to the image in the popup
   $('.popup img').attr("src", imagesrc );
 
+  // BVB show the popup
   $('.popup').show();
 
 });
